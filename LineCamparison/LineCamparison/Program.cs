304a39887fff1cda5eedacc6dd8e057a8242addb
Your UserName <aratinaik151997@gmail.com>
2021-11-18 19:44:21 +0530

    Added feature to check equality of lines

--- LineCamparison/LineCamparison/Program.cs
+++ LineCamparison/LineCamparison/Program.cs
@@ -12,17 +12,36 @@ namespace LineCamparison
         {
             Console.WriteLine("Line Camparison Computation problem");
             double x1, y1,x2,y2;
-            double lenght = 0;
+            double lenghtFirst = 0;
+            double lenghtSecond = 0;
+            Console.WriteLine("First Line");
             Console.WriteLine("Enter Starting point of line x1 and y1 ordinate");
             x1 =Convert.ToDouble(Console.ReadLine());
             y1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter End point of line x2 and y2 ordinate");
             x2 = Convert.ToDouble(Console.ReadLine());
             y2 = Convert.ToDouble(Console.ReadLine());
-            double powx = Math.Pow(x2-x1, 2);
-            double powy = Math.Pow(y2-y1, 2);
-            lenght = Math.Sqrt(powx + powy);
-            Console.WriteLine("Lenght of line is " + lenght);
+            double powFirst = Math.Pow(x2 - x1, 2);
+            double powSecond = Math.Pow(y2 - y1, 2);
+            lenghtFirst = Math.Sqrt(powFirst + powSecond);
+            Console.WriteLine("Lenght of First line is " + lenghtFirst);
+
+            Console.WriteLine("Second Line");
+            Console.WriteLine("Enter Starting point of line x1 and y1 ordinate");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter End point of line x2 and y2 ordinate");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            double powx = Math.Pow(x2 - x1, 2);
+            double powy = Math.Pow(y2 - y1, 2);
+            lenghtSecond = Math.Sqrt(powx + powy);
+            Console.WriteLine("Lenght of Second line is " + lenghtSecond);
+
+            if (lenghtFirst.Equals(lenghtSecond))
+                Console.WriteLine("Two lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
             Console.ReadKey();
         }
     }
